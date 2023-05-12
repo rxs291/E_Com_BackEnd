91260e82b667936cba3817124c56783c21a45bab
Ki Sar <rxs291@yahoo.com>
2023-05-12 16:02:41 -0500

    moved files to push server.js and README to root

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./Develop/routes");
 const sequelize = require("./config/connection");
 // import sequelize connection
 
